05798ffb9e1c020df69a0fa51409bf834328f734
huytq@ics-p.vn <huytq@ics-p.vn>
2024-07-16 14:28:37 +0700

    CRUD Roles

--- src/KnowledgeSpace.BackendServer/Startup.cs
+++ src/KnowledgeSpace.BackendServer/Startup.cs
@@ -2,19 +2,13 @@ using KnowledgeSpace.BackendServer.Data;
 using KnowledgeSpace.BackendServer.Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KnowledgeSpace.BackendServer
 {
---
+++ src/KnowledgeSpace.ViewModels/Pagination.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace KnowledgeSpace.ViewModels
+{
+    public class Pagination<T>
+    {
+        public List<T> Items { get; set; }
+
+        public int TotalRecords { get; set; }
+    }
+}
