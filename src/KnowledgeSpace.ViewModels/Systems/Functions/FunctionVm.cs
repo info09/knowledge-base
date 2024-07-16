081b96dfe89ae6875944c36195dda4c9e63edf97
huytq@ics-p.vn <huytq@ics-p.vn>
2024-07-16 16:59:57 +0700

    Function API

---
+++ src/KnowledgeSpace.ViewModels/Systems/Functions/FunctionVm.cs
@@ -0,0 +1,15 @@
+﻿namespace KnowledgeSpace.ViewModels.Systems.Functions
+{
+    public class FunctionVm
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public string ParentId { get; set; }
+    }
+}
