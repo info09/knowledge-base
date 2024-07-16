3d9e22cacf656f628fa37092cb2eec53665c83f8
huytq@ics-p.vn <huytq@ics-p.vn>
2024-07-16 16:30:42 +0700

    Fix bug and add swagger authen

--- src/KnowledgeSpace.BackendServer/Controllers/RolesController.cs
+++ src/KnowledgeSpace.BackendServer/Controllers/RolesController.cs
@@ -1,5 +1,6 @@
 ﻿using KnowledgeSpace.ViewModels;
 using KnowledgeSpace.ViewModels.Systems.Roles;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ namespace KnowledgeSpace.BackendServer.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize("Bearer")]
     public class RolesController : ControllerBase
     {
         private readonly RoleManager<IdentityRole> _roleManager;
--- src/KnowledgeSpace.BackendServer/Startup.cs
+++ src/KnowledgeSpace.BackendServer/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 
 namespace KnowledgeSpace.BackendServer
 {
@@ -45,7 +46,8 @@ namespace KnowledgeSpace.BackendServer
             .AddInMemoryApiResources(Config.Apis)
             .AddInMemoryClients(Config.Clients)
             .AddInMemoryIdentityResources(Config.Ids)
-            .AddAspNetIdentity<User>();
+            .AddAspNetIdentity<User>()
+            .AddDeveloperSigningCredential();
 
             services.Configure<IdentityOptions>(options =>
             {
@@ -64,11 +66,60 @@ namespace KnowledgeSpace.BackendServer
 
             services.AddControllersWithViews().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<RoleVmValidator>());
 
-            services.AddRazorPages();
+            services.AddAuthentication().AddLocalApi("Bearer", option =>
+            {
+                option.ExpectedScope = "api.knowledgespace";
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Bearer", policy =>
+                {
+                    policy.AddAuthenticationSchemes("Bearer");
+                    policy.RequireAuthenticatedUser();
+                });
+            });
+
+            services.AddRazorPages(options =>
+            {
+                options.Conventions.AddAreaFolderRouteModelConvention("Identity", "/Account/", model =>
+                {
+                    foreach (var selector in model.Selectors)
+                    {
+                        var attributeRouteModel = selector.AttributeRouteModel;
+                        attributeRouteModel.Order = -1;
+                        attributeRouteModel.Template = attributeRouteModel.Template.Remove(0, "Identity".Length);
+                    }
+                });
+            });
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "KnowledgeSpace.BackendServer", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Implicit = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri("https://localhost:5000/connect/authorize"),
+                            Scopes = new Dictionary<string, string> { { "api.knowledgespace", "KnowledgeSpace API" } }
+                        }
+                    }
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                        },
+                        new List<string>{ "api.knowledgespace" }
+                    }
+                });
             });
 
             services.AddTransient<DbInitializer>();
@@ -82,7 +133,11 @@ namespace KnowledgeSpace.BackendServer
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "KnowledgeSpace.BackendServer v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "KnowledgeSpace.BackendServer v1");
+                    c.OAuthClientId("swagger");
+                });
             }
 
             app.UseStaticFiles();
