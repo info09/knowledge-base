7ff96ed0aa33bae097c0e146fb7a2eab672fcac7
huytq@ics-p.vn <huytq@ics-p.vn>
2024-07-16 17:02:22 +0700

    Command API

---
+++ src/KnowledgeSpace.BackendServer/Controllers/CommandsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnowledgeSpace.BackendServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+    }
+}
---
+++ src/KnowledgeSpace.ViewModels/Systems/Commands/AddCommandToFunctionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace KnowledgeSpace.ViewModels.Systems.Commands
+{
+    public class AddCommandToFunctionRequest
+    {
+        public string CommandId { get; set; }
+
+        public string FunctionId { get; set; }
+    }
+}
