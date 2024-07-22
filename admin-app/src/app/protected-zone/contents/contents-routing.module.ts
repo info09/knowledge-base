cb7e8a4b5bbac87e7a37d52c8d3f6b9a9c8ba858
huytq@ics-p.vn <huytq@ics-p.vn>
2024-07-22 16:11:35 +0700

    Create Angular Structure

---
+++ admin-app/src/app/protected-zone/contents/contents-routing.module.ts
@@ -0,0 +1,35 @@
+import { RouterModule, Routes } from '@angular/router';
+import { KnowledgeBasesComponent } from './knowledge-bases/knowledge-bases.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ReportsComponent } from './reports/reports.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: KnowledgeBasesComponent
+    },
+    {
+        path: 'knowledge-base',
+        component: KnowledgeBasesComponent
+    },
+    {
+        path: 'categories',
+        component: CategoriesComponent
+    },
+    {
+        path: 'comments',
+        component: CommentsComponent
+    },
+    {
+        path: 'reports',
+        component: ReportsComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class ContentsRoutingModule {}
